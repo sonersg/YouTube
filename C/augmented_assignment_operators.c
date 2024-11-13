ff57491ab51b2e1671b348a93f9d4a559be1762f
sonersg <sonersg@outlook.com>
2024-11-13 14:54:36 +0300

    augmented-assignment

---
+++ C/augmented_assignment_operators.c
@@ -0,0 +1,26 @@
+
+// AUGMENTED ASSIGNMENT OPERATORS:
+// help us to reassign values in a short and fast way.
+
+#include <stdio.h>
+
+int main()
+{
+    int age = 22;
+
+    // age = age + 2;
+    // age += 2;
+
+    // age = age - 2;
+    // age -= 2;
+
+    // age = age * 2;
+    // age *= 2;
+
+    // age = age / 2;
+    age /= 2;
+
+    printf("%d \n", age);
+
+    return 0;
+}
---
+++ C/increment_decrement_operators.c
@@ -0,0 +1,22 @@
+// INCREMENT and DECREMENT OPERATORS
+
+// ++
+// --
+
+#include <stdio.h>
+
+int main()
+{
+    int number = 0;
+
+    printf("%d \n", number);
+
+    // number = number + 1;
+
+    // number++;
+    number--;
+
+    printf("%d \n", number);
+
+    return 0;
+}
